b4e0418dc4b9a4d2ecccaec487e8c2d8135d3c96
Prachi Nikhare <82951655+prachinikhare@users.noreply.github.com>
2022-02-24 00:04:51 +0530

    Update application.py

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ db = SQLAlchemy(application)
 class FEEDBACKPAGE(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    email = db.Column(db.String(500), nullable=False)
+    email = db.Column(db.Varchar(500), nullable=False)
     message = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
